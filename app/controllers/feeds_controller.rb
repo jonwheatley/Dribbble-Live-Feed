2d5c70a2415fce4ce206cccc190daffbd0ae0811
Jon Wheatley <jonwheatley@gmail.com>
2011-08-16 20:52:39 -0700

    done the hard part

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -4,6 +4,13 @@ class FeedsController < ApplicationController
   
   def index
     
+    # pull shit from dribbble
+
+
+    shot_pull = open("http://api.dribbble.com/shots/everyone").read
+    convert_to_json = JSON::parse(shot_pull)
+    @shot_pulled = Hashie::Mash.new(convert_to_json)
+    
   end
   
 end
